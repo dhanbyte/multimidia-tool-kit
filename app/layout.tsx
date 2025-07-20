8356f2564fff98dadec041131ca0b440d6856902
Dhananjay singh <dhananjay.win2004@gmail.com>
2025-07-20 19:15:39 +0530

    Add dynamic sitemap API

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CSRTEPL9GN"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-CSRTEPL9GN');
+</script>
         {/* ✅ Favicon */}
         <link
           rel="icon"
