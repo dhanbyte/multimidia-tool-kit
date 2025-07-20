ebd753efc2c6a429af2d6f8164b8366515af060f
Dhananjay singh <dhananjay.win2004@gmail.com>
2025-07-20 17:31:01 +0530

    Add dynamic sitemap APIsgg  bfxbdfhdfhdrhsrhfhroute

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-
+import { Drawer as DrawerPrimitive } from "vaul" // ✅ ये लाइन जरूरी है
 import { cn } from "@/lib/utils"
 
 const Drawer = ({
@@ -16,9 +16,7 @@ const Drawer = ({
 Drawer.displayName = "Drawer"
 
 const DrawerTrigger = DrawerPrimitive.Trigger
-
 const DrawerPortal = DrawerPrimitive.Portal
-
 const DrawerClose = DrawerPrimitive.Close
 
 const DrawerOverlay = React.forwardRef<
