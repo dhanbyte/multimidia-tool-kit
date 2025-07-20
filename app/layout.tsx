4ece4d04d3b2f055ddec547acf68f0c78b97414f
Dhananjay singh <dhananjay.win2004@gmail.com>
2025-07-20 17:36:14 +0530

    Add dynamic sitemap APIsgg  bfxbdfhdfhdrhsrhfhrodsfute

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,12 @@ export default function RootLayout({
       <head>
         <link rel="icon" type="image/png" href="https://ik.imagekit.io/b5qewhvhb/New%20Folder/ChatGPT%20Image%20Jul%2020,%202025,%2011_38_49%20AM.png?updatedAt=1752991986819" />
 
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TJBS3J62');</script>
+
         {/* ✅ Google Analytics script */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-CSRTEPL9GN"></script>
         <script
@@ -66,6 +72,8 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJBS3J62"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <SiteHeader />
           <main className="flex-1">
