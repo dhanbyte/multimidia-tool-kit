9864b71d9de14da8ff08b44973cd818423dc3aa0
dhanbyte <dhananjay.win2004@gmail.com>
2025-09-18 15:10:57 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,15 @@ export default function RootLayout({
           crossOrigin="anonymous"
         ></script>
         <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WMB2H955');`,
+          }}
+        />
+        <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-LE6FGHJQ8G"
         ></script>
@@ -106,7 +115,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TJBS3J62"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WMB2H955"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
